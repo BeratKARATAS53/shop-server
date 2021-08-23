6474448de8eeecbf0f2ba536c7de5219f088916b
Berat KARATAŞ <b21527142@cs.hacettepe.edu.tr>
2021-08-23 21:39:51 +0300

    One update

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ app.get('/api/orders', (req, res) => {
 
 app.post('/api/orders', (req, res) => {
 	console.log("POST", req.body);
-
-	let newOrder = {id : nextOrderId, count : +req.body.count, cost : +req.body.cost};
+	
+	let newOrder = {id : nextOrderId, count : +req.body.count, cost : +req.body.cost, basket : req.body.basket};
 	orders.push(newOrder);
 	nextOrderId++;
 	res.send(newOrder);
